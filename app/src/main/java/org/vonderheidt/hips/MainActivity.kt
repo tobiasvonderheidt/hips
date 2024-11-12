32e2b54a9c1157eba70ff5bfe51462b68a43a407
Tobias Vonderheidt <tobias@vonderheidt.org>
2024-11-12 23:44:06 +0100

    Remove enableEdgeToEdge()

--- app/src/main/java/org/vonderheidt/hips/MainActivity.kt
+++ app/src/main/java/org/vonderheidt/hips/MainActivity.kt
@@ -3,7 +3,6 @@ package org.vonderheidt.hips
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -20,9 +19,6 @@ class MainActivity : ComponentActivity() {
     // Boilerplate code
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Since Android 15, all apps are enforced to go edge-to-edge
-        enableEdgeToEdge()
 
         // Set MainScreen function as content of the main screen
         setContent {
