2b6666283b5b8a474a6eb6785ccbb09ce846f381
Tobias Vonderheidt <tobias@vonderheidt.org>
2024-11-12 23:51:30 +0100

    Add context, secret message and cover text fields

--- app/src/main/java/org/vonderheidt/hips/MainActivity.kt
+++ app/src/main/java/org/vonderheidt/hips/MainActivity.kt
@@ -3,13 +3,26 @@ package org.vonderheidt.hips
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.vonderheidt.hips.ui.theme.HiPSTheme
 
 /**
@@ -40,10 +53,52 @@ class MainActivity : ComponentActivity() {
  */
 @Composable
 fun MainScreen(modifier: Modifier) {
-    Text(
-        text = "Hello World!",
-        modifier = modifier
-    )
+    // State variables
+    var context by rememberSaveable { mutableStateOf("") }
+    var secretMessage by rememberSaveable { mutableStateOf("") }
+    var coverText by rememberSaveable { mutableStateOf("") }
+
+    // UI components
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // 1st input is context
+        OutlinedTextField(
+            value = context,
+            onValueChange = { context = it },
+            modifier = modifier.fillMaxWidth(0.8f),
+            label = { Text(text = "Context") }
+        )
+
+        Spacer(modifier = modifier.height(32.dp))
+
+        // 2nd input is secret message
+        OutlinedTextField(
+            value = secretMessage,
+            onValueChange = { secretMessage = it },
+            modifier = modifier.fillMaxWidth(0.8f),
+            label = { Text(text = "Secret message") }
+        )
+
+        Spacer(modifier = modifier.height(32.dp))
+
+        // Start button
+        Button(
+            onClick = { coverText = "Encode of $secretMessage using $context" },
+            shape = RoundedCornerShape(4.dp)
+        ) {
+            Text(text = "Start")
+        }
+
+        Spacer(modifier = modifier.height(32.dp))
+
+        // Output is cover text
+        Text(
+            text = coverText,
+            modifier = modifier.fillMaxWidth(0.8f)
+        )
+    }
 }
 
 /**
