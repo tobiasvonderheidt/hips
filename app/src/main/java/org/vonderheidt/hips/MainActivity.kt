c136973d4f290cc1f62cc7a9246356672498b01c
Tobias Vonderheidt <tobias@vonderheidt.org>
2024-11-13 13:37:43 +0100

    Add encode/decode mode selector

--- app/src/main/java/org/vonderheidt/hips/MainActivity.kt
+++ app/src/main/java/org/vonderheidt/hips/MainActivity.kt
@@ -3,7 +3,9 @@ package org.vonderheidt.hips
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,14 +15,19 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.vonderheidt.hips.ui.theme.HiPSTheme
@@ -56,6 +63,8 @@ fun MainScreen(modifier: Modifier) {
     // State variables
     var context by rememberSaveable { mutableStateOf("") }
     var secretMessage by rememberSaveable { mutableStateOf("") }
+    val modes = listOf("Encode", "Decode")
+    var selectedMode by rememberSaveable { mutableIntStateOf(0) }
     var coverText by rememberSaveable { mutableStateOf("") }
 
     // UI components
@@ -73,31 +82,97 @@ fun MainScreen(modifier: Modifier) {
 
         Spacer(modifier = modifier.height(32.dp))
 
-        // 2nd input is secret message
-        OutlinedTextField(
-            value = secretMessage,
-            onValueChange = { secretMessage = it },
-            modifier = modifier.fillMaxWidth(0.8f),
-            label = { Text(text = "Secret message") }
-        )
-
-        Spacer(modifier = modifier.height(32.dp))
-
-        // Start button
-        Button(
-            onClick = { coverText = "Encode of $secretMessage using $context" },
-            shape = RoundedCornerShape(4.dp)
-        ) {
-            Text(text = "Start")
+        // 2nd input is either secret message or cover text
+        if (selectedMode == 0) {
+            OutlinedTextField(
+                value = secretMessage,
+                onValueChange = { secretMessage = it },
+                modifier = modifier.fillMaxWidth(0.8f),
+                label = { Text(text = "Secret message") }
+            )
+        }
+        else {
+            OutlinedTextField(
+                value = coverText,
+                onValueChange = { coverText = it },
+                modifier = modifier.fillMaxWidth(0.8f),
+                label = { Text(text = "Cover text") }
+            )
         }
 
         Spacer(modifier = modifier.height(32.dp))
 
-        // Output is cover text
-        Text(
-            text = coverText,
-            modifier = modifier.fillMaxWidth(0.8f)
-        )
+        Row(
+            modifier = modifier.fillMaxWidth(0.8f),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            // Mode selector
+            SingleChoiceSegmentedButtonRow {
+                // Mostly follows example given in docs
+                modes.forEachIndexed { index, _ ->
+                    SegmentedButton(
+                        selected = index == selectedMode,
+                        onClick = {
+                            // Clear both secret message and cover text when mode is changed
+                            selectedMode = index
+                            secretMessage = ""
+                            coverText = ""
+                        },
+                        shape = SegmentedButtonDefaults.itemShape(
+                            index = index,
+                            count = modes.size,
+                            baseShape = RoundedCornerShape(4.dp)
+                        ),
+                        label = { Text(text = modes[index]) }
+                    )
+                }
+            }
+
+            // Start button
+            Button(
+                onClick = {
+                    if (selectedMode == 0) {
+                        coverText = "Encode of $secretMessage using $context"
+                    }
+                    else {
+                        secretMessage = "Decode of $coverText using $context"
+                    }
+                },
+                shape = RoundedCornerShape(4.dp)
+            ) {
+                Text(text = "Start")
+            }
+        }
+
+        Spacer(modifier = modifier.height(32.dp))
+
+        // Output is either cover text or secret message
+        if (selectedMode == 0) {
+            Text(
+                text = "Cover text:",
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = modifier.height(16.dp))
+
+            Text(
+                text = coverText,
+                modifier = modifier.fillMaxWidth(0.8f)
+            )
+        }
+        else {
+            Text(
+                text = "Secret message:",
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = modifier.height(16.dp))
+
+            Text(
+                text = secretMessage,
+                modifier = modifier.fillMaxWidth(0.8f)
+            )
+        }
     }
 }
 
