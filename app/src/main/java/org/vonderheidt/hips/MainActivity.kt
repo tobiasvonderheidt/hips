d23df31c4a5502a5025d765ddcff0577495bf859
Tobias Vonderheidt <tobias@vonderheidt.org>
2024-11-13 15:13:41 +0100

    Make MainScreen scrollable for large text inputs

--- app/src/main/java/org/vonderheidt/hips/MainActivity.kt
+++ app/src/main/java/org/vonderheidt/hips/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material.icons.outlined.Email
@@ -74,9 +76,14 @@ fun MainScreen(modifier: Modifier) {
     var selectedMode by rememberSaveable { mutableIntStateOf(0) }
     var coverText by rememberSaveable { mutableStateOf("") }
 
+    // Scrolling
+    val scrollState = rememberScrollState()
+
     // UI components
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(state = scrollState),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // 1st input is context
