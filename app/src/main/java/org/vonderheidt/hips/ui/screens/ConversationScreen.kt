437baccace49eafbf48967ccffaaaccce1f0dc8b
Tobias Vonderheidt <tobias@vonderheidt.org>
2024-11-18 16:04:05 +0100

    Show messages on conversation screen

--- app/src/main/java/org/vonderheidt/hips/ui/screens/ConversationScreen.kt
+++ app/src/main/java/org/vonderheidt/hips/ui/screens/ConversationScreen.kt
@@ -1,21 +1,37 @@
 package org.vonderheidt.hips.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowForward
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import org.vonderheidt.hips.data.Message
 import org.vonderheidt.hips.navigation.Screen
 import org.vonderheidt.hips.ui.theme.HiPSTheme
 
@@ -24,6 +40,9 @@ import org.vonderheidt.hips.ui.theme.HiPSTheme
  */
 @Composable
 fun ConversationScreen(navController: NavController, modifier: Modifier) {
+    // State variables
+    var messages by rememberSaveable { mutableStateOf(listOf<Message>()) }
+
     // UI components
     Column(
         modifier = modifier.fillMaxSize(),
@@ -50,6 +69,38 @@ fun ConversationScreen(navController: NavController, modifier: Modifier) {
                     imageVector = Icons.AutoMirrored.Outlined.ArrowForward,
                     contentDescription = "Go back to home screen"
                 )
+            }
+        }
+
+        // Messages
+        // Use LazyColumn as it only loads visible messages into memory, allowing for arbitrary number of messages
+        LazyColumn(
+            modifier = modifier.fillMaxWidth(0.95f)
+        ) {
+            // Current user (senderID == 0) is right aligned and green
+            // Chat partners (senderID != 0) are left aligned and red
+            items(messages) { message ->
+                Row (
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = if (message.senderID == 0) Arrangement.End else Arrangement.Start
+                ) {
+                    Box(
+                        modifier = modifier
+                            .fillMaxWidth(0.9f)
+                            .background(
+                                color = if (message.senderID == 0) Color(0xFF2E7D32) else Color(0xFFB71C1C),
+                                shape = RoundedCornerShape(4.dp)
+                            )
+                            .padding(8.dp)
+                    ) {
+                        Text(
+                            text = message.content,
+                            color = Color.White
+                        )
+                    }
+                }
+
+                Spacer(modifier = modifier.height(8.dp))
             }
         }
     }
