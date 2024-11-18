f93cb53a12a8892fdb18a536092655c77bcbca17
Tobias Vonderheidt <tobias@vonderheidt.org>
2024-11-18 14:01:06 +0100

    Add back button to conversation screen

--- app/src/main/java/org/vonderheidt/hips/ui/screens/ConversationScreen.kt
+++ app/src/main/java/org/vonderheidt/hips/ui/screens/ConversationScreen.kt
@@ -1,12 +1,22 @@
 package org.vonderheidt.hips.ui.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowForward
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import org.vonderheidt.hips.navigation.Screen
 import org.vonderheidt.hips.ui.theme.HiPSTheme
 
 /**
@@ -14,7 +24,35 @@ import org.vonderheidt.hips.ui.theme.HiPSTheme
  */
 @Composable
 fun ConversationScreen(navController: NavController, modifier: Modifier) {
-    Text(text = "Hello World!")
+    // UI components
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Back button
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+            IconButton(
+                onClick = {
+                    // Navigate back to home screen
+                    navController.navigate(Screen.Home.route) {
+                        // Empty back stack, including home screen
+                        // Otherwise app won't close when user goes back once more via the phone's back button
+                        popUpTo(Screen.Home.route) {
+                            inclusive = true
+                        }
+                    }
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Outlined.ArrowForward,
+                    contentDescription = "Go back to home screen"
+                )
+            }
+        }
+    }
 }
 
 /**
