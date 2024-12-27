e3bdc10c6bc4e2be42eda17911a77159357957db
Tobias Vonderheidt <tobias@vonderheidt.org>
2024-12-27 10:05:17 +0100

    Add enums for conversion and steganography modes

---
+++ app/src/main/java/org/vonderheidt/hips/utils/ConversionMode.kt
@@ -0,0 +1,20 @@
+package org.vonderheidt.hips.utils
+
+/**
+ * Class to enumerate all conversion modes.
+ *
+ * @param displayName Display name of the conversion mode.
+ */
+enum class ConversionMode(private val displayName: String) {
+    Arithmetic("Arithmetic (recommended)"),
+    UTF8("UTF-8");
+
+    /**
+     * Function to get the display name of the conversion mode.
+     *
+     * @return Display name of the conversion mode.
+     */
+    override fun toString(): String {
+        return displayName
+    }
+}
