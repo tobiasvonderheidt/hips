ce07bcd951380922010bc63c4d90c532eb2c90cd
Tobias Vonderheidt <tobias@vonderheidt.org>
2025-01-04 13:38:37 +0100

    Define basic structure in Steganography object

--- app/src/main/java/org/vonderheidt/hips/utils/Steganography.kt
+++ app/src/main/java/org/vonderheidt/hips/utils/Steganography.kt
@@ -1,6 +1,6 @@
 package org.vonderheidt.hips.utils
 
-import kotlinx.coroutines.delay
+import org.vonderheidt.hips.data.Settings
 
 /**
  * Object (i.e. singleton class) that represents steganography encoding and decoding.
@@ -9,22 +9,56 @@ object Steganography {
     /**
      * Function to encode secret message into cover text using given context.
      */
-    suspend fun encode(context: String, secretMessage: String): String {
-        // Wait 5 seconds
-        delay(5000)
+    suspend fun encode(
+        context: String,
+        secretMessage: String,
+        conversionMode: ConversionMode = Settings.conversionMode,
+        steganographyMode: SteganographyMode = Settings.steganographyMode
+    ): String {
+        // Step 1: Convert secret message to a (compressed) binary representation
+        val plainBits = when (conversionMode) {
+            ConversionMode.Arithmetic -> { Arithmetic.decode("", secretMessage) }   // Stegasuras: Arithmetic binary conversion is just decoding with empty context
+            ConversionMode.UTF8 -> { UTF8.encode(secretMessage) }
+        }
 
-        // Return placeholder string
-        return "Encode of $secretMessage using $context"
+        // Step 2: Encrypt binary representation of secret message
+        val cipherBits = Crypto.encrypt(plainBits)
+
+        // Step 3: Encode encrypted binary representation of secret message into cover text
+        val coverText = when (steganographyMode) {
+            SteganographyMode.Arithmetic -> { Arithmetic.encode(context, cipherBits) }
+            SteganographyMode.Bins -> { Bins.encode(context, cipherBits) }
+            SteganographyMode.Huffman -> { Huffman.encode(context, cipherBits) }
+        }
+
+        return coverText
     }
 
     /**
      * Function to decode secret message from cover text using given context.
-    */
-    suspend fun decode(context: String, coverText: String): String {
-        // Wait 5 seconds
-        delay(5000)
+     */
+    suspend fun decode(
+        context: String,
+        coverText: String,
+        conversionMode: ConversionMode = Settings.conversionMode,
+        steganographyMode: SteganographyMode = Settings.steganographyMode
+    ): String {
+        // Invert step 3
+        val cipherBits = when (steganographyMode) {
+            SteganographyMode.Arithmetic -> { Arithmetic.decode(context, coverText) }
+            SteganographyMode.Bins -> { Bins.decode(context, coverText) }
+            SteganographyMode.Huffman -> { Huffman.decode(context, coverText) }
+        }
 
-        // Return placeholder string
-        return "Decode of $coverText using $context"
+        // Invert step 2
+        val plainBits = Crypto.decrypt(cipherBits)
+
+        // Invert step 1
+        val secretMessage = when (conversionMode) {
+            ConversionMode.Arithmetic -> { Arithmetic.encode("", plainBits) }   // Stegasuras: Arithmetic string conversion is just encoding with empty context
+            ConversionMode.UTF8 -> { UTF8.decode(plainBits) }
+        }
+
+        return secretMessage
     }
 }
