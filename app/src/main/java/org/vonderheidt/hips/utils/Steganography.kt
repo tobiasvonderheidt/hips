8b83cac9b551fa99d390b5dd9445e666dcabb68a
Tobias Vonderheidt <tobias@vonderheidt.org>
2025-01-16 14:27:55 +0100

    Solve decoding of greedy sampling in last sentence

--- app/src/main/java/org/vonderheidt/hips/utils/Steganography.kt
+++ app/src/main/java/org/vonderheidt/hips/utils/Steganography.kt
@@ -15,10 +15,13 @@ object Steganography {
         conversionMode: ConversionMode = Settings.conversionMode,
         steganographyMode: SteganographyMode = Settings.steganographyMode
     ): String {
+        // Step 0: Prepare secret message by appending ASCII NUL character
+        val preparedSecretMessage = prepare(secretMessage)
+
         // Step 1: Convert secret message to a (compressed) binary representation
         val plainBits = when (conversionMode) {
-            ConversionMode.Arithmetic -> { Arithmetic.decode("", secretMessage) }   // Stegasuras: Arithmetic binary conversion is just decoding with empty context
-            ConversionMode.UTF8 -> { UTF8.encode(secretMessage) }
+            ConversionMode.Arithmetic -> { Arithmetic.decode("", preparedSecretMessage) }   // Stegasuras: Arithmetic binary conversion is just decoding with empty context
+            ConversionMode.UTF8 -> { UTF8.encode(preparedSecretMessage) }
         }
 
         // Step 2: Encrypt binary representation of secret message
@@ -54,10 +57,45 @@ object Steganography {
         val plainBits = Crypto.decrypt(cipherBits)
 
         // Invert step 1
-        val secretMessage = when (conversionMode) {
+        val preparedSecretMessage = when (conversionMode) {
             ConversionMode.Arithmetic -> { Arithmetic.encode("", plainBits) }   // Stegasuras: Arithmetic string conversion is just encoding with empty context
             ConversionMode.UTF8 -> { UTF8.decode(plainBits) }
         }
+
+        // Invert step 0
+        val secretMessage = unprepare(preparedSecretMessage)
+
+        return secretMessage
+    }
+
+    /**
+     * Function to prepare a secret message for binary encoding.
+     *
+     * Appends the ASCII NUL character to the original secret message. Needed to remove artefacts from greedy sampling after binary decoding.
+     *
+     * @param secretMessage A secret message.
+     * @return The prepared secret message.
+     */
+    private fun prepare(secretMessage: String): String {
+        // Kotlin doesn't use NUL-terminated strings, instead makes them immutable and stores length
+        // So using NUL as a character in a Kotlin string can't cause any collisions
+        val preparedSecretMessage = secretMessage + '\u0000'
+
+        return preparedSecretMessage
+    }
+
+    /**
+     * Function to unprepare a secret message after binary decoding.
+     *
+     * Strips the ASCII NUL character and everything after it. Therefore removes any artefacts from greedy sampling, rendering the original secret message.
+     *
+     * @param preparedSecretMessage A prepared secret message.
+     * @return The secret message.
+     */
+    private fun unprepare(preparedSecretMessage: String): String {
+        val secretMessage = preparedSecretMessage
+            .split('\u0000')
+            .first()
 
         return secretMessage
     }
