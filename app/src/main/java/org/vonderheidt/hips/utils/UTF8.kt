e7907f2c181f97c71f40f7ab64d30f110a512047
Tobias Vonderheidt <tobias@vonderheidt.org>
2025-01-16 18:49:12 +0100

    Update JavaDoc

--- app/src/main/java/org/vonderheidt/hips/utils/UTF8.kt
+++ app/src/main/java/org/vonderheidt/hips/utils/UTF8.kt
@@ -9,11 +9,11 @@ object UTF8 {
     /**
      * Function to convert a string into its binary representation using UTF-8 encoding.
      *
-     * @param secretMessage Secret message.
-     * @return Binary representation of the secret message.
+     * @param preparedSecretMessage A prepared secret message.
+     * @return The binary representation of the prepared secret message.
      */
-    fun encode(secretMessage: String): ByteArray {
-        val plainBits = secretMessage.toByteArray(charset = Charsets.UTF_8)
+    fun encode(preparedSecretMessage: String): ByteArray {
+        val plainBits = (preparedSecretMessage).toByteArray(charset = Charsets.UTF_8)
 
         return plainBits
     }
@@ -21,12 +21,12 @@ object UTF8 {
     /**
      * Function to convert the binary representation of a string back to the string using UTF-8 decoding.
      *
-     * @param plainBits Binary representation of the secret message.
-     * @return Secret message.
+     * @param plainBits The binary representation of a prepared secret message.
+     * @return The prepared secret message.
      */
     fun decode(plainBits: ByteArray): String {
-        val secretMessage = String(bytes = plainBits, charset = Charsets.UTF_8)
+        val preparedSecretMessage = String(bytes = plainBits, charset = Charsets.UTF_8)
 
-        return secretMessage
+        return preparedSecretMessage
     }
 }
