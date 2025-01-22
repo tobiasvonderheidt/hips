71c484fe43eee4f8d4d25b9be207f5fd44c706c9
Tobias Vonderheidt <tobias@vonderheidt.org>
2025-01-22 10:15:37 +0100

    Create data class for users

---
+++ app/src/main/java/org/vonderheidt/hips/data/User.kt
@@ -0,0 +1,12 @@
+package org.vonderheidt.hips.data
+
+/**
+ * Class that represents a user.
+ *
+ * @param id The user ID.
+ * @param name A user name.
+ */
+data class User(
+    val id: Int,
+    val name: String
+)
