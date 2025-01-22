dabd383e52baebbb51572c710e1430ccb12730d0
Tobias Vonderheidt <tobias@vonderheidt.org>
2025-01-22 13:30:06 +0100

    Add annotations for Room to user data class

--- app/src/main/java/org/vonderheidt/hips/data/User.kt
+++ app/src/main/java/org/vonderheidt/hips/data/User.kt
@@ -1,12 +1,16 @@
 package org.vonderheidt.hips.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 /**
  * Class that represents a user.
  *
  * @param id The user ID.
  * @param name A user name.
  */
+@Entity
 data class User(
-    val id: Int,
+    @PrimaryKey val id: Int,
     val name: String
 )
