41cfde9eb09ff489ed3681e88d577e1ff00b969b
Tobias Vonderheidt <tobias@vonderheidt.org>
2025-03-10 00:30:42 +0100

    Refactor navigation

--- app/src/main/java/org/vonderheidt/hips/navigation/NavGraph.kt
+++ app/src/main/java/org/vonderheidt/hips/navigation/NavGraph.kt
@@ -2,7 +2,6 @@ package org.vonderheidt.hips.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -11,18 +10,25 @@ import org.vonderheidt.hips.ui.screens.HomeScreen
 import org.vonderheidt.hips.ui.screens.SettingsScreen
 
 /**
- * Function that initializes the navigation.
+ * Object (i.e. singleton class) that represents the navigation graph.
  */
-@Composable
-fun SetupNavGraph(modifier: Modifier) {
-    val navController: NavHostController = rememberNavController()
+object NavGraph {
+    /**
+     * Function to initialize the navigation.
+     *
+     * @param modifier The modifier from MainActivity.
+     */
+    @Composable
+    fun Setup(modifier: Modifier) {
+        val navController = rememberNavController()
 
-    NavHost(
-        navController = navController,
-        startDestination = Screen.Home.route
-    ) {
-        composable(Screen.Home.route) { HomeScreen(navController, modifier) }
-        composable(Screen.Settings.route) { SettingsScreen(navController, modifier) }
-        composable(Screen.Conversation.route) { ConversationScreen(navController, modifier) }
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route
+        ) {
+            composable(Screen.Home.route) { HomeScreen(navController, modifier) }
+            composable(Screen.Settings.route) { SettingsScreen(navController, modifier) }
+            composable(Screen.Conversation.route) { ConversationScreen(navController, modifier) }
+        }
     }
 }
