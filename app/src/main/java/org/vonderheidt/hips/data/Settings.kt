e6dcecc318854f186dd43a194aeb41efdc35c102
Tobias Vonderheidt <tobias@vonderheidt.org>
2025-03-10 16:08:51 +0100

    Add function to reset settings to their defaults

--- app/src/main/java/org/vonderheidt/hips/data/Settings.kt
+++ app/src/main/java/org/vonderheidt/hips/data/Settings.kt
@@ -1,23 +1,58 @@
 package org.vonderheidt.hips.data
 
 import org.vonderheidt.hips.utils.ConversionMode
+import org.vonderheidt.hips.utils.LlamaCpp
 import org.vonderheidt.hips.utils.SteganographyMode
 
 /**
  * Object (i.e. singleton class) that represents the user settings. Holds default values to be set upon installation of this app.
  */
 object Settings {
-    var conversionMode = ConversionMode.Arithmetic
-    var systemPrompt = """
+    // Define default values
+    private val defaultConversionMode = ConversionMode.Arithmetic
+    private val defaultSystemPrompt = """
         You and I are friends, talking about what we did on the weekend.
         Use phrases and abbreviations typical for chat messages.
         Be brief and casual, but friendly and engaging.
     """.trimIndent().replace("\n", " ")
-    var numberOfMessages = 0
-    var steganographyMode = SteganographyMode.Arithmetic
-    var temperature = 0.9f
-    var topK = 0             // Only used if LLM is not in memory
-    var precision = 0        // Only used if LLM is not in memory
-    var blockSize = 3
-    var bitsPerToken = 3
+    private val defaultNumberOfMessages = 0
+    private val defaultSteganographyMode = SteganographyMode.Arithmetic
+    private val defaultTemperature = 0.9f
+    private val defaultTopK = 0             // Only used if LLM is not in memory
+    private val defaultPrecision = 0        // Only used if LLM is not in memory
+    private val defaultBlockSize = 3
+    private val defaultBitsPerToken = 3
+
+    // Initialize current values with defaults
+    var conversionMode = defaultConversionMode
+    var systemPrompt = defaultSystemPrompt
+    var numberOfMessages = defaultNumberOfMessages
+    var steganographyMode = defaultSteganographyMode
+    var temperature = defaultTemperature
+    var topK = defaultTopK
+    var precision = defaultPrecision
+    var blockSize = defaultBlockSize
+    var bitsPerToken = defaultBitsPerToken
+
+    /**
+     * Function to reset the settings to their default values.
+     *
+     * @param general Boolean that is true if general settings are to be reset, false otherwise.
+     * @param llm Boolean that is true if settings specific to the LLM are to be reset, false otherwise.
+     */
+    fun reset(general: Boolean, llm: Boolean) {
+        if (general) {
+            conversionMode = defaultConversionMode
+            systemPrompt = defaultSystemPrompt
+            numberOfMessages = defaultNumberOfMessages
+            steganographyMode = defaultSteganographyMode
+            temperature = defaultTemperature
+            blockSize = defaultBlockSize
+            bitsPerToken = defaultBitsPerToken
+        }
+        if (llm) {
+            topK = if (LlamaCpp.isInMemory()) LlamaCpp.getVocabSize() else defaultTopK
+            precision = defaultPrecision
+        }
+    }
 }
