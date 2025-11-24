6ce1863c82663af0ed218e48526162b1d52e150d
Tobias Vonderheidt <tobias@vonderheidt.org>
2025-11-24 22:43:14 +0100

    Upgrade {compile,target}Sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "org.vonderheidt.hips"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "org.vonderheidt.hips"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
