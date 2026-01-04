a0b735232381a93627e981fc95e9473809eb446b
Tobias Vonderheidt <tobias@vonderheidt.org>
2026-01-04 22:10:53 +0100

    Translate Format to C++
    Now also what's needed for Arithmetic

--- app/src/main/cpp/Format.h
+++ app/src/main/cpp/Format.h
@@ -28,6 +28,47 @@ public:
      * @return The Java ByteArray.
      */
     static jbyteArray asByteArray(JNIEnv* env, std::vector<bool> bitVector);
+
+    /**
+     * Function to format a Java ByteArray as a bit vector. Assumes that the ByteArray is 0-padded and that the first byte stores the length of the padding in bits.
+     * Removes both the padding length and the padding.
+     *
+     * @param env The JNI environment.
+     * @param jByteArray A Java ByteArray, 0-padded with length of padding in bits stored in first byte.
+     * @return The bit vector, with padding removed.
+     */
+    static std::vector<bool> asBitVectorWithoutPadding(JNIEnv* env, jbyteArray jByteArray);
+
+    /**
+     * Function to reverse formatting of a padded Java ByteArray as a bit vector (i.e. to reverse `Format::asBitVectorWithoutPadding(JNIEnv*, jbyteArray)`).
+     * Adds 0-padding at the start so that length of bit vector is multiple of 8. Prepends a byte that stores length of padding in bits.
+     *
+     * @param env The JNI environment.
+     * @param bitVector A bit vector.
+     * @return The Java ByteArray, 0-padded with length of padding in bits stored in first byte.
+     */
+    static jbyteArray asByteArrayWithPadding(JNIEnv* env, const std::vector<bool>& bitVector);
+
+    /**
+     * Function to format a long long number as a bit vector of desired length. Pads the bit vector with leading 0s if needed.
+     *
+     * Corresponds to Stegasuras method `int2bits` in `utils.py`. Parameter `inp` was renamed to `loong`, `num_bits` to `numberOfBits`.
+     *
+     * @param loong A long long number.
+     * @param numberOfBits The desired length of the bit vector.
+     * @return The bit vector.
+     */
+    static std::vector<bool> asBitVector(long long loong, int numberOfBits);
+
+    /**
+     * Function to reverse formatting of a long long number as a bit vector (i.e. to reverse `Format::asBitVector(long long, int)`).
+     *
+     * Corresponds to Stegasuras method `bits2int` in `utils.py`. Parameter `bits` was renamed to `bitVector`.
+     *
+     * @param bitVector A bit vector containing a long long number.
+     * @return The long long number.
+     */
+    static long long asLong(const std::vector<bool>& bitVector);
 };
 
 #endif
