2eaf1242e2c7b18d83e34fb92b4b5e36535d66d3
Tobias Vonderheidt <tobias@vonderheidt.org>
2026-04-09 23:47:01 +0200

    Update docs

--- app/src/main/java/org/vonderheidt/hips/compression/Compression.kt
+++ app/src/main/java/org/vonderheidt/hips/compression/Compression.kt
@@ -3,9 +3,16 @@ package org.vonderheidt.hips.compression
 import org.vonderheidt.hips.bitmage.BitString
 
 /**
- * Singleton that provides convenience wrappers for text compression
+ * Object (i.e. singleton class) that provides convenience wrappers for compression of the secret message from string to binary and vice versa.
  */
 object Compression {
+    /**
+     * Function to compress a secret message from string to binary.
+     *
+     * @param secretMessage The secret message to compress.
+     * @param compressionMode The compression mode to use.
+     * @return The binary representation of the secret message.
+     */
     fun compress(secretMessage: String, compressionMode: CompressionMode): BitString {
         val plainBits = when (compressionMode) {
             CompressionMode.Adaptive -> Adaptive.compress(secretMessage)
@@ -17,6 +24,13 @@ object Compression {
         return plainBits
     }
 
+    /**
+     * Function to decompress a secret message from binary to string.
+     *
+     * @param plainBits The binary representation of the secret message.
+     * @param compressionMode The compression mode to use.
+     * @return The decompressed secret message.
+     */
     fun decompress(plainBits: BitString, compressionMode: CompressionMode): String {
         val secretMessage = when (compressionMode) {
             CompressionMode.Adaptive -> Adaptive.decompress(plainBits)
