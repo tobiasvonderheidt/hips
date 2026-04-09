ea53724e7fa18a07b7cbc07697d1fce3c8977441
Tobias Vonderheidt <tobias@vonderheidt.org>
2026-04-09 22:35:04 +0200

    Update docs

--- app/src/main/java/org/vonderheidt/hips/compression/CompressionProvider.kt
+++ app/src/main/java/org/vonderheidt/hips/compression/CompressionProvider.kt
@@ -2,20 +2,23 @@ package org.vonderheidt.hips.compression
 
 import org.vonderheidt.hips.bitmage.BitString
 
+/**
+ * Interface that declares functions for compressing a secret message from string to binary and vice versa.
+ */
 interface CompressionProvider {
     /**
-     * Compresses plaintext to minimum number of bits before steganography
+     * Function to compress a secret message from string to binary.
      *
-     * @param message A plaintext message to compress
-     * @return The binary representation of the prepared secret message.
+     * @param message The secret message to compress.
+     * @return The binary representation of the secret message.
      */
     fun compress(message: String): BitString
 
     /**
-     * Inflates compressed plaintext bits to readable message
+     * Function to decompress a secret message from binary to string.
      *
-     * @param plainBits Binary representation of compressed message.
-     * @return Uncompressed plaintext message.
+     * @param plainBits The binary representation of the secret message.
+     * @return The decompressed secret message.
      */
     fun inflate(plainBits: BitString): String
 }
