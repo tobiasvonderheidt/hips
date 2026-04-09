7096d4968de0b12656afc1ae07ae709213001299
Tobias Vonderheidt <tobias@vonderheidt.org>
2026-04-09 19:25:16 +0200

    Remove debug logging

--- app/src/main/java/org/vonderheidt/hips/bitmage/BitString.kt
+++ app/src/main/java/org/vonderheidt/hips/bitmage/BitString.kt
@@ -1,6 +1,5 @@
 package org.vonderheidt.hips.bitmage
 
-import android.util.Log
 import kotlin.math.ceil
 import kotlin.math.min
 
@@ -68,23 +67,19 @@ class BitString(fragment: BitFragment) {
                 outLen += part.bitLength
             }
             else {
-                //println("trying to take $remaining bits from ${part.bitLength} bit part")
                 val bytesToTake = ceil(remaining.toDouble() / 8).toInt()
                 val leftoverBits = 8 - (remaining % 8)
 
-                //println("taking $bytesToTake bytes, leaving $leftoverBits in split byte")
 
                 val takenBytes = part.bytes.untilIndex(bytesToTake)
                 val nonTakenBytes = part.bytes.fromIndex(bytesToTake)
 
-                //println("taken: ${takenBytes.hex()} nonTaken: ${nonTakenBytes.hex()}")
 
                 val restOfPart = BitString(nonTakenBytes, part.bitLength - takenBytes.size * 8)
 
                 if(leftoverBits != 8) {
                     val splitByte = part.bytes[bytesToTake-1].toUByte().toInt()
                     val bits = (splitByte shl (remaining % 8)) and 0xff
-                    //println("split byte is ${splitByte.toString(16)}, leftover bits are ${bits.toString(2).padStart(8, '0')}")
                     restOfPart.prepend(BitFragment(byteArrayOf(bits.toByte()), leftoverBits))
                 }
 
@@ -108,27 +103,22 @@ class BitString(fragment: BitFragment) {
             var bitOffsetInPart = 0
             var currentByte: UByte
             var bitsRemainingInPartByte: Int
-            //println("\n\n")
             while (bitOffsetInPart < part.bitLength) {
                 currentByte = part.bytes[bitOffsetInPart / 8].toUByte()
                 bitsRemainingInPartByte = 8 - (bitOffsetInPart % 8)
                 val contentRemainingInPartByte = min(bitsRemainingInPartByte, part.bitLength - bitOffsetInPart)
 
-                //println("bitOffsetInPart $bitOffsetInPart bitsRemainingInPartByte $bitsRemainingInPartByte bitsRemainingInOutputByte $bitsRemainingInOutputByte")
 
                 val inputBitMask = (1 shl bitsRemainingInPartByte) - 1
                 val nextInputBits = (currentByte.toInt() and inputBitMask)
-                //println("input byte ${currentByte.toString(2).padStart(8, '0')} mask ${inputBitMask.toString(2).padStart(8, '0')} input bits: ${nextInputBits.toString(2).padStart(8, '0')}")
 
                 // fully pack the current output byte
                 if(contentRemainingInPartByte >= bitsRemainingInOutputByte) {
                     val packableBits = nextInputBits shr (bitsRemainingInPartByte - bitsRemainingInOutputByte)
-                    //println("packable bits: ${packableBits.toString(2).padStart(bitsRemainingInOutputByte, '0')}")
 
                     outputByte = outputByte or packableBits
                     bitOffsetInPart += bitsRemainingInOutputByte
 
-                    //println("filled output byte: ${outputByte.toString(2).padStart(8, '0')}")
                     bytes += (outputByte and 0xff).toByte()
                     outputByte = 0
                     bitsRemainingInOutputByte = 8
@@ -136,10 +126,7 @@ class BitString(fragment: BitFragment) {
                 // partially pack output byte with full remainder of input byte
                 else {
                     val packableBits = nextInputBits shr (bitsRemainingInPartByte - contentRemainingInPartByte)
-                    //println("packable bits: ${packableBits.toString(2).padStart(contentRemainingInPartByte, '0')}")
-                    //println("output byte has ${8-bitsRemainingInOutputByte} bits occupied, we have $contentRemainingInPartByte bits to pack, shift left by ${(bitsRemainingInOutputByte - contentRemainingInPartByte)}")
                     outputByte = outputByte or (packableBits shl (bitsRemainingInOutputByte - contentRemainingInPartByte))
-                    //println("partial output byte: ${outputByte.toString(2).padStart(8, '0')}")
 
                     bitsRemainingInOutputByte -= contentRemainingInPartByte
 
@@ -162,15 +149,12 @@ class BitString(fragment: BitFragment) {
         val needleFragment = sequence.toBitFragment()
         val needleSize = needleFragment.bitLength
 
-        //Log.d("BitString", "searching for $needleSize b needle in $haystackSize b haystack")
 
         var searchIndex = haystackSize - needleSize
         var matchIndex = 0
         while(searchIndex > 0) {
-            //Log.d("BitString", "searching at bit offset $searchIndex")
             while(haystackFragment.getBit(searchIndex+matchIndex) == needleFragment.getBit(matchIndex)) {
                 matchIndex += 1
-                //Log.d("BitString", "$matchIndex bit match")
                 if(matchIndex == needleSize)
                     return searchIndex
             }
