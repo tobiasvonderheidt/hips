2c532888907203c6c4b80f4d848e9249e23f40bf
Tobias Vonderheidt <tobias@vonderheidt.org>
2026-04-09 22:46:55 +0200

    Remove redundant case

--- app/src/main/java/org/vonderheidt/hips/compression/Compression.kt
+++ app/src/main/java/org/vonderheidt/hips/compression/Compression.kt
@@ -12,7 +12,6 @@ object Compression {
             CompressionMode.Arithmetic -> ArithmeticCompression.compress(secretMessage)
             CompressionMode.UTF8 -> UTF8.compress(secretMessage)
             CompressionMode.BitCrush -> BitCrush.compress(secretMessage)
-            else -> throw Exception("unsupported compression mode: $mode")
         }
 
         return compressedBits
@@ -24,7 +23,6 @@ object Compression {
             CompressionMode.Arithmetic -> ArithmeticCompression.decompress(bits)
             CompressionMode.UTF8 -> UTF8.decompress(bits)
             CompressionMode.BitCrush -> BitCrush.decompress(bits)
-            else -> throw Exception("unsupported compression mode: $mode")
         }
 
         return uncompressed
