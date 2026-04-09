3a036ef89589f10cd52d1dda5acf64ccb92495cb
Tobias Vonderheidt <tobias@vonderheidt.org>
2026-04-10 00:24:46 +0200

    Refactor to fix inconsistent variable names

--- app/src/main/java/org/vonderheidt/hips/compression/ArithmeticCompression.kt
+++ app/src/main/java/org/vonderheidt/hips/compression/ArithmeticCompression.kt
@@ -15,24 +15,25 @@ object ArithmeticCompression : CompressionProvider {
      * @return The compressed binary representation of the secret message.
      */
     override fun compress(secretMessage: String): BitString {
+        // Reset ctx
         LlamaCpp.resetInstance()
+
         // Stegasuras:
         // Arithmetic compression is just decoding with empty context
         // Parameters temperature, topK and precision are not taken from settings, but hard-coded to use the unmodulated LLM
         // While topK is set to the vocabulary size of the LLM, precision is set as high as possible so (ideally) no tokens have probability < 1/2^precision
-        val bytes = Arithmetic.decode(
+        val plainBits = Arithmetic.decode(
             context = "".toByteArray(charset = Charsets.UTF_8),
             coverText = secretMessage.toByteArray(charset = Charsets.UTF_8),
             temperature = 1.0f,
             topK = LlamaCpp.getVocabSize(),
             precision = 40
-        )
+        ).let { BitString(it, it.size * 8) }
 
-        val bits = BitString(bytes, bytes.size * 8)
-        val paddingLen = bits.takeFew(8).toInt()
-        bits.takeFew(paddingLen)
+        val paddingLength = plainBits.takeFew(8).toInt()
+        plainBits.takeFew(paddingLength)
 
-        return bits
+        return plainBits
     }
 
     /**
@@ -42,25 +43,22 @@ object ArithmeticCompression : CompressionProvider {
      * @return The secret message.
      */
     override fun decompress(plainBits: BitString): String {
-
         // Reset ctx
         LlamaCpp.resetInstance()
 
         // Stegasuras:
         // Arithmetic decompression is just encoding with empty context
         // Same parameters as compression
-        val compressedBytes = plainBits.toBitFragment()
-
-        val decodedBytes = Arithmetic.encode(
+        val secretMessage = Arithmetic.encode(
             context = "".toByteArray(charset = Charsets.UTF_8),
-            cipherBits = compressedBytes.bytes,
-            bitLength = compressedBytes.bitLength,
+            cipherBits = plainBits.toBitFragment().bytes,
+            bitLength = plainBits.toBitFragment().bitLength,
             temperature = 1.0f,
             topK = LlamaCpp.getVocabSize(),
             precision = 40,
             isResumed = false
-        )
+        ).decodeToString()
 
-        return decodedBytes.decodeToString()
+        return secretMessage
     }
 }
