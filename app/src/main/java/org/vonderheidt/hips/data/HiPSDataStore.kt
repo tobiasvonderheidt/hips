5279f392c2c726ac3529a4fca120233c38a0e09b
Tobias Vonderheidt <tobias@vonderheidt.org>
2026-04-10 16:20:33 +0200

    Remove unused import

--- app/src/main/java/org/vonderheidt/hips/data/HiPSDataStore.kt
+++ app/src/main/java/org/vonderheidt/hips/data/HiPSDataStore.kt
@@ -11,7 +11,6 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
-import org.vonderheidt.hips.compression.Compression
 import org.vonderheidt.hips.compression.CompressionMode
 import org.vonderheidt.hips.utils.SteganographyMode
 
